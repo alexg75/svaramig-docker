613d882ce4f979820eb7bd84d3934c57afe81afd
alexgrimaldi <alexgrimaldi@gmail.com>
2025-10-13 17:49:06 +0100

    replace XXX_HOST_XXX and XXX_PORT_XXX in remote-properties.json for svaramig-web-client

--- svaramig-web-client/build-image.sh
+++ svaramig-web-client/build-image.sh
@@ -3,10 +3,15 @@ if [ -z "$1" ]
     echo "No svaramig-web-client version supplied"
     exit 1
 fi
-readonly IMAGE_VERSION=1
+readonly IMAGE_VERSION=2
+readonly REMOTE_SERVICE_HOST=rp-app
+readonly REMOTE_SERVICE_PORT=8080
 
 rm -fR svaramig
 cp -a . ../../svaramig-web-client/svaramig .
+
+sed -i.bak "s/XXX_HOST_XXX/${REMOTE_SERVICE_HOST}/g" ./svaramig/src/utils/remote-properties.json
+sed -i.bak "s/XXX_PORT_XXX/${REMOTE_SERVICE_PORT}/g" ./svaramig/src/utils/remote-properties.json
 
 docker build --tag=svaramig-web-client:$1.$IMAGE_VERSION .
 
