ae3cb9e2edcdfe95c2a3b359c48cf5f9038c8f8d
alexgrimaldi <alexgrimaldi@gmail.com>
2025-10-14 12:59:25 +0100

    allow to generate docker image of the web client that works locally

--- svaramig-web-client/build-image.sh
+++ svaramig-web-client/build-image.sh
@@ -1,11 +1,19 @@
-if [ -z "$1" ]
-  then
-    echo "No svaramig-web-client version supplied"
-    exit 1
-fi
 readonly IMAGE_VERSION=2
-readonly REMOTE_SERVICE_HOST=rp-app
-readonly REMOTE_SERVICE_PORT=8080
+REMOTE_SERVICE_HOST=rp-app
+REMOTE_SERVICE_PORT=8080
+
+if [ -z "$1" ]; then
+  echo "No svaramig-web-client version supplied"
+  exit 1
+fi
+
+if [ -z "$2" ]; then
+  echo "No environment specified. Default to LIVE"
+elif [[ "$2"=="LOCAL" ]]; then
+  REMOTE_SERVICE_HOST=192.168.178.177
+  REMOTE_SERVICE_PORT=8080    
+fi
+echo Building for backend $REMOTE_SERVICE_HOST:$REMOTE_SERVICE_PORT
 
 rm -fR svaramig
 cp -a . ../../svaramig-web-client/svaramig .
@@ -14,6 +22,12 @@ sed -i.bak "s/XXX_HOST_XXX/${REMOTE_SERVICE_HOST}/g" ./svaramig/src/utils/remote
 sed -i.bak "s/XXX_PORT_XXX/${REMOTE_SERVICE_PORT}/g" ./svaramig/src/utils/remote-properties.json
 
 docker build --tag=svaramig-web-client:$1.$IMAGE_VERSION .
+
+if [[ "$2"=="LOCAL" ]]; then
+  docker image tag svaramig-web-client:$1.$IMAGE_VERSION rp-db:5000/svaramig-web-client:$1.$IMAGE_VERSION.LOCAL
+  echo "Local images do not get pushed to the registry"
+  exit 0
+fi
 
 docker image tag svaramig-web-client:$1.$IMAGE_VERSION rp-db:5000/svaramig-web-client:$1.$IMAGE_VERSION
 
