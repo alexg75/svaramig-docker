af6afe6c337ad6eadb1b90bf2e80bd88b531f525
alexgrimaldi <alexgrimaldi@gmail.com>
2025-10-20 16:18:35 +0100

    add tokenUtils.py to iot-slack-token-sync build scripts

--- iot-slack-token-sync/build-image.sh
+++ iot-slack-token-sync/build-image.sh
@@ -15,6 +15,8 @@ cp -p ../../iot-slack-token-sync/refresh-token.py  ./target
 cp -p ../../iot-slack-token-sync/requirements.txt  ./target
 cp -p ../../iot-slack-token-sync/token.json  ./target
 cp -p ../../iot-common/logger.py ./target
+cp -p ../../iot-common/tokenUtils.py ./target
+
 
 docker build --tag=$SERVICE_NAME:$1.$IMAGE_VERSION .
 
