afa0a86ff632ed759d14909511567bb7d3952142
alexgrimaldi <alexgrimaldi@gmail.com>
2025-10-22 12:35:32 +0100

    iot-consumer-error

---
+++ iot-consumer-error/target/tokenUtils.py
@@ -0,0 +1,27 @@
+import logger
+import json
+
+ACCESS_TOKEN = "access_token"
+REFRESH_TOKEN = "refresh_token"
+TOKEN_FILE = "/token.json"
+TOPIC_NAME = 'slack-token'
+
+log = logger.setup_logger("iot-common")
+
+def load_stored_token():
+    token_dict = {}
+    try:        
+        json_file = open(TOKEN_FILE, "r")        
+        token_dict = json.load(json_file)
+        log.info(f"TOKEN LOADED: {token_dict}")
+    except Exception as e:
+        log.error("Could not load stored token")
+        log.error(e)
+    return token_dict
+
+def save_tokens(token_dict):
+    try:
+        with open(TOKEN_FILE, "w") as outfile:
+            json.dump(token_dict, outfile)
+    except:
+        log.error("Could not save token")
