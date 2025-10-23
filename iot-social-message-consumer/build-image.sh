c05c4c77df4346f1df368126a43313ff5123074a
alexgrimaldi <alexgrimaldi@gmail.com>
2025-10-23 09:03:37 +0100

    remove target from iot-social-message-consumer

--- iot-social-message-consumer/build-image.sh
+++ iot-social-message-consumer/build-image.sh
@@ -17,21 +17,22 @@ cp -p ../../iot-social-message-consumer/slackClient.py  ./target
 cp -p ../../iot-social-message-consumer/start-consumers.sh  ./target
 cp -p ../../iot-social-message-consumer/requirements.txt  ./target
 cp -p ../../iot-social-message-consumer/token.json  ./target
-cp -p ../../iot-common/logger.py ./target
-cp -p ../../iot-common/tokenUtils.py ./target
+cp -p ../../iot-common/*.py ./target
 
 chmod 775 ./target/start-consumers.sh
 
-docker build --tag=$SERVICE_NAME:$1.$IMAGE_VERSION .
+docker build --tag=test .
 
-if [[ "${2}" = "LOCAL" ]]; then
-  docker image tag $SERVICE_NAME:$1.$IMAGE_VERSION $REGISTRY_HOST:$REGISTRY_PORT/$SERVICE_NAME:$1.$IMAGE_VERSION.LOCAL
-  echo "Local images do not get pushed to the registry"
-  exit 0
-fi
+# docker build --tag=$SERVICE_NAME:$1.$IMAGE_VERSION .
 
-docker image tag $SERVICE_NAME:$1.$IMAGE_VERSION $REGISTRY_HOST:$REGISTRY_PORT/$SERVICE_NAME:$1.$IMAGE_VERSION
+# if [[ "${2}" = "LOCAL" ]]; then
+#   docker image tag $SERVICE_NAME:$1.$IMAGE_VERSION $REGISTRY_HOST:$REGISTRY_PORT/$SERVICE_NAME:$1.$IMAGE_VERSION.LOCAL
+#   echo "Local images do not get pushed to the registry"
+#   exit 0
+# fi
 
-docker image push $REGISTRY_HOST:$REGISTRY_PORT/$SERVICE_NAME:$1.$IMAGE_VERSION
+# docker image tag $SERVICE_NAME:$1.$IMAGE_VERSION $REGISTRY_HOST:$REGISTRY_PORT/$SERVICE_NAME:$1.$IMAGE_VERSION
 
-docker push $REGISTRY_HOST:$REGISTRY_PORT/$SERVICE_NAME:$1.$IMAGE_VERSION
+# docker image push $REGISTRY_HOST:$REGISTRY_PORT/$SERVICE_NAME:$1.$IMAGE_VERSION
+
+# docker push $REGISTRY_HOST:$REGISTRY_PORT/$SERVICE_NAME:$1.$IMAGE_VERSION
--- iot-social-message-consumer/target/start-consumers.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-exec python3 /consumer-slack-token.py &
-exec python3 /consumer-social-message.py
