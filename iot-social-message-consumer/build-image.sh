e929dcdb7bba44433c533fe51dbef10100abd30d
alexgrimaldi <alexgrimaldi@gmail.com>
2025-10-23 10:32:17 +0100

    enable iot-social-message-consumer to work

--- iot-social-message-consumer/build-image.sh
+++ iot-social-message-consumer/build-image.sh
@@ -1,4 +1,4 @@
-readonly IMAGE_VERSION=1
+readonly IMAGE_VERSION=2
 readonly SERVICE_NAME=iot-social-message-consumer
 readonly REGISTRY_HOST=rp-db
 readonly REGISTRY_PORT=5000
@@ -16,23 +16,20 @@ cp -p ../../iot-social-message-consumer/consumer-social-message.py  ./target
 cp -p ../../iot-social-message-consumer/slackClient.py  ./target
 cp -p ../../iot-social-message-consumer/start-consumers.sh  ./target
 cp -p ../../iot-social-message-consumer/requirements.txt  ./target
-cp -p ../../iot-social-message-consumer/token.json  ./target
 cp -p ../../iot-common/*.py ./target
 
 chmod 775 ./target/start-consumers.sh
 
-docker build --tag=test .
+docker build --tag=$SERVICE_NAME:$1.$IMAGE_VERSION .
 
-# docker build --tag=$SERVICE_NAME:$1.$IMAGE_VERSION .
+if [[ "${2}" = "LOCAL" ]]; then
+  docker image tag $SERVICE_NAME:$1.$IMAGE_VERSION $REGISTRY_HOST:$REGISTRY_PORT/$SERVICE_NAME:$1.$IMAGE_VERSION.LOCAL
+  echo "Local images do not get pushed to the registry"
+  exit 0
+fi
 
-# if [[ "${2}" = "LOCAL" ]]; then
-#   docker image tag $SERVICE_NAME:$1.$IMAGE_VERSION $REGISTRY_HOST:$REGISTRY_PORT/$SERVICE_NAME:$1.$IMAGE_VERSION.LOCAL
-#   echo "Local images do not get pushed to the registry"
-#   exit 0
-# fi
+docker image tag $SERVICE_NAME:$1.$IMAGE_VERSION $REGISTRY_HOST:$REGISTRY_PORT/$SERVICE_NAME:$1.$IMAGE_VERSION
 
-# docker image tag $SERVICE_NAME:$1.$IMAGE_VERSION $REGISTRY_HOST:$REGISTRY_PORT/$SERVICE_NAME:$1.$IMAGE_VERSION
+docker image push $REGISTRY_HOST:$REGISTRY_PORT/$SERVICE_NAME:$1.$IMAGE_VERSION
 
-# docker image push $REGISTRY_HOST:$REGISTRY_PORT/$SERVICE_NAME:$1.$IMAGE_VERSION
-
-# docker push $REGISTRY_HOST:$REGISTRY_PORT/$SERVICE_NAME:$1.$IMAGE_VERSION
+docker push $REGISTRY_HOST:$REGISTRY_PORT/$SERVICE_NAME:$1.$IMAGE_VERSION
